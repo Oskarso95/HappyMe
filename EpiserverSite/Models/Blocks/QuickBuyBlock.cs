e0dac590f79bcab0f779065ae69ca9452756efd3
oskarso95 <oskarso95@gmail.com>
2021-08-11 15:44:58 +0200

    changes..

---
+++ EpiserverSite/Models/Blocks/QuickBuyBlock.cs
@@ -0,0 +1,32 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations;
+using EpiserverSite.Models.Pages;
+
+namespace EpiserverSite.Views.Shared.Blocks
+{
+    [ContentType(DisplayName = "QuickBuyBlock", GUID = "270e3342-0ded-4dce-80ca-3914dd668c57",
+        Description = "Block to trigger sales for product")]
+    public class QuickBuyBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Product",
+            Description = "Link to product page",
+            GroupName = SystemTabNames.Content,
+            Order = 100)]
+        [AllowedTypes(typeof(ProductPage))]
+        [Required]
+        public virtual ContentReference Product { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Button text",
+            Description = "Fallback text for button",
+            GroupName = SystemTabNames.Content,
+            Order = 200)]
+        public virtual string ButtonText { get; set; }
+    }
+}
---
+++ EpiserverSite/Models/Blocks/QuoteBlock.cs
@@ -0,0 +1,49 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Web;
+
+namespace EpiserverSite.Models.Blocks
+{
+    [ContentType(DisplayName = "QuoteBlock", GUID = "170cac93-4fd0-484b-b622-ae31ada53d43",
+        Description = "Displays quote")]
+    public class QuoteBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Text",
+            Description = "Quote text",
+            GroupName = SystemTabNames.Content,
+            Order = 100)]
+        [Required]
+        public virtual XhtmlString Text { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Image",
+            Description = "Image of quote block",
+            GroupName = SystemTabNames.Content,
+            Order = 200)]
+        [UIHint(UIHint.Image)]
+        public virtual ContentReference Image { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Background image",
+            Description = "Background-image of quote block",
+            GroupName = SystemTabNames.Content,
+            Order = 300)]
+        [UIHint(UIHint.Image)]
+        public virtual ContentReference BackgroundImage { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Author",
+            Description = "Authors name",
+            GroupName = SystemTabNames.Content,
+            Order = 400)]
+        public virtual string Author { get; set; }
+    }
+}
